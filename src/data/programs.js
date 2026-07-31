f9ed3b3c5956438158ad16e11541eadcef5b6ad7
ryan-reshadi <ryan@reshadi.com>
2026-07-30 23:12:13 -0700

    updated # of students reached to 150

--- src/data/programs.js
+++ src/data/programs.js
@@ -33,7 +33,7 @@ export const programs = [
 
 export const impactStats = [
   {
-    value: 100,
+    value: 150,
     suffix: '+',
     label: 'Students Reached',
     desc: 'Across workshops and events',
